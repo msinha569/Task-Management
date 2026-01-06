750a7124068d1fd8078c2b49c89b7a42e4a7ef54
msinha569 <msinha569@gmail.com>
2026-01-06 13:55:50 +0530

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ export const app = express()
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://task-management-4bh0.onrender.com"],
     credentials: true,
   })
 );
